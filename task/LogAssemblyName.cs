e856b804595c726f4338111e86074c5df561b2c6
Jonathan Peppers <jonathan.peppers@microsoft.com>
2019-04-04 15:07:05 -0500

    Repro of msbuild + mono issue

---
+++ task/LogAssemblyName.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace task
+{
+	public class LogAssemblyName : Task
+	{
+		public override bool Execute ()
+		{
+			using (var reader = new PEReader (File.OpenRead (GetType ().Assembly.Location))) {
+				var metadata = reader.GetMetadataReader ();
+				var assembly = metadata.GetAssemblyDefinition ();
+				var name = assembly.GetAssemblyName ();
+				Log.LogMessage (MessageImportance.High, $"Assembly Name: {name}");
+			}
+			return true;
+		}
+	}
+}
